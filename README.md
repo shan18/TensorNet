93a589a6d806ba2ecab89bf014312149655f6eea
Shantanu <thegeek.004@gmail.com>
2020-03-15 19:26:33 +0530

    Refactor files for packaging

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# TensorNet
+# TensorNet v0.0.1
 
-A high-level deep learning framework built on top of PyTorch.
+[Under development]
 
-## Setup Instructions
+A high-level deep learning library built on top of PyTorch.
 
-The project is still under development. So for now, in order to use the framework, copy the contents of the project's root folder into your project's root and import the modules. Examples to use the framework are present in the [examples](examples/) directory.
+## Installation
+
+`$ pip install tensornet`
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def requirements():
+    with open('requirements.txt', 'r') as f:
+        return f.read().splitlines()
+
+
+setup(
+    name='torch-tensornet',
+    version='0.0.1',
+    author='Shantanu Acharya',
+    author_email='thegeek.004@gmail.com',
+    description='A high-level deep learning library build on top of PyTorch',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/shan18/TensorNet',
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=requirements()
+)
