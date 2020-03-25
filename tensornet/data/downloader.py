3e3a9d1983cc06da88a704bd101cf800d001227c
Shantanu <thegeek.004@gmail.com>
2020-03-25 17:50:04 +0530

    v0.0.8: Add MNIST dataset
        - __init__, README: Version update
        - data: include MNIST dataset

--- tensornet/data/downloader.py
+++ tensornet/data/downloader.py
@@ -24,3 +24,26 @@ def download_cifar10(path=None, train=True, transform=None):
     return datasets.CIFAR10(
         path, train=train, download=True, transform=transform
     )
+
+
+def download_mnist(path=None, train=True, transform=None):
+    """Download MNIST dataset
+
+    Args:
+        path (str, optional): Path where dataset will be downloaded.
+            If no path provided, data will be downloaded in a pre-defined
+            directory. (default: None)
+        train (bool, optional): If True, download the training data else
+            download the test data. (default: True)
+        transform (tensornet.Transformations, optional): Data transformations
+            to be applied on the data. (default: None)
+    
+    Returns:
+        Downloaded dataset.
+    """
+
+    if path is None:
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mnist')
+    return datasets.MNIST(
+        path, train=train, download=True, transform=transform
+    )
