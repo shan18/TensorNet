Fix bug in image normalization and unnormalization

tensornet/data/utils.py
@@ -15,12 +15,28 @@ def unnormalize(image, mean, std, transpose=False):
             be returned. This param is effective only when image is
             a tensor. If tensor, the output will have channel number
             as the last dim. (default: False)
+    
+    Returns:
+        Unnormalized image
     """
+
+    # Check if image is tensor, convert to numpy array
+    tensor = False
     if type(image) == torch.Tensor:  # tensor
-        if transpose and len(image.size()) == 3:
+        tensor = True
+        if len(image.size()) == 3:
             image = image.transpose(0, 1).transpose(1, 2)
         image = np.array(image)
+    
+    # Perform normalization
     image = image * std + mean
+
+    # Convert image back to its original data type
+    if tensor:
+        if not transpose and len(image.shape) == 3:
+            image = np.transpose(image, (2, 0, 1))
+        image = torch.Tensor(image)
+    
     return image
 
 
@@ -38,12 +54,28 @@ def normalize(image, mean, std, transpose=False):
             be returned. This param is effective only when image is
             a tensor. If tensor, the output will have channel number
             as the last dim. (default: False)
+    
+    Returns:
+        Normalized image
     """
+
+    # Check if image is tensor, convert to numpy array
+    tensor = False
     if type(image) == torch.Tensor:  # tensor
-        if transpose and len(image.size()) == 3:
+        tensor = True
+        if len(image.size()) == 3:
             image = image.transpose(0, 1).transpose(1, 2)
         image = np.array(image)
+    
+    # Perform normalization
     image = (image - mean) / std
+
+    # Convert image back to its original data type
+    if tensor:
+        if not transpose and len(image.shape) == 3:
+            image = np.transpose(image, (2, 0, 1))
+        image = torch.Tensor(image)
+
     return image
 
 
@@ -52,6 +84,9 @@ def to_numpy(tensor):
 
     Args:
         tensor (torch.Tensor): Tensor to be converted.
+    
+    Returns:
+        numpy.ndarray
     """
     return tensor.transpose(0, 1).transpose(1, 2).clone().numpy()
 
@@ -61,5 +96,8 @@ def to_tensor(ndarray):
 
     Args:
         ndarray (numpy.ndarray): Array to be converted.
+    
+    Returns:
+        torch.Tensor
     """
     return torch.Tensor(np.transpose(ndarray, (2, 0, 1)))