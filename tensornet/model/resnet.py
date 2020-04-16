95e55d09e18610055b5465ed774ed94d1a3dae79
Shantanu <thegeek.004@gmail.com>
2020-04-16 16:47:45 +0530

    Replace AvgPool2d with AdaptiveAvgPool and option to specify num_classes in Resnet

--- tensornet/model/resnet.py
+++ tensornet/model/resnet.py
@@ -51,6 +51,7 @@ class ResNet(BaseModel):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.gap = nn.AdaptiveAvgPool2d(1)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
@@ -76,11 +77,11 @@ class ResNet(BaseModel):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
+        out = self.gap(out)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
 
 
-def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
+def ResNet18(num_classes=10):
+    return ResNet(BasicBlock, [2, 2, 2, 2], num_classes)
