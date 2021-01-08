16596fc96f021351aec360bb6d5965984ed9a69a
Shantanu Acharya <thegeek.004@gmail.com>
2021-01-09 01:20:18 +0530

    add resnet variations in models module

--- tensornet/models/__init__.py
+++ tensornet/models/__init__.py
@@ -1,5 +1,9 @@
 from .basicnet import BasicNet
-from .resnet import ResNet18
+from .resnet import (
+    resnet18, resnet34, resnet50, resnet101, resnet152,
+    resnext50_32x4d, resnext101_32x8d, wide_resnet50_2,
+    wide_resnet101_2,
+)
 from .masknet import MaskNet
 from .dsresnet import DSResNet
 from .mobilenetv2 import MobileNetV2, mobilenet_v2
