d783e7da45b5d112fe74db832531f4e0b8a6d741
Shantanu Acharya <thegeek.004@gmail.com>
2021-11-27 18:37:57 -0500

    add support for shufflenet v2

--- tensornet/models/__init__.py
+++ tensornet/models/__init__.py
@@ -7,6 +7,7 @@ from .resnet import (
 from .dsresnet import DSResNet
 from .mobilenetv2 import MobileNetV2, mobilenet_v2
 from .squeezenet import SqueezeNet, squeezenet1_0, squeezenet1_1
+from .shufflenetv2 import ShuffleNetV2, shufflenet_v2_x0_5, shufflenet_v2_x1_0
 
 
 __all__ = [
@@ -14,4 +15,5 @@ __all__ = [
     'resnet101', 'resnet152', 'resnext50_32x4d', 'resnext101_32x8d',
     'wide_resnet50_2', 'wide_resnet101_2', 'MobileNetV2',
     'mobilenet_v2', 'DSResNet', 'SqueezeNet', 'squeezenet1_0', 'squeezenet1_1',
+    'ShuffleNetV2', 'shufflenet_v2_x0_5', 'shufflenet_v2_x1_0',
 ]
